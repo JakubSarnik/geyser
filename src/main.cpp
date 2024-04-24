9b313e2672921363d562c4651da9baf4a02523c1
Jakub Šárník <jakub.sarnik@gmail.com>
2024-04-25 01:16:34 +0200

    Build CaDiCaL and test it.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+#include "cadical.hpp"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, CaDiCaL!\n";
+
+    auto solver = CaDiCaL::Solver{};
+
+    int tie = 1;
+    int shirt = 2;
+
+    solver.add (-tie), solver.add (shirt), solver.add (0);
+    solver.add (tie), solver.add (shirt), solver.add (0);
+    solver.add (-tie), solver.add (-shirt), solver.add (0);
+
+    int res = solver.solve();
+
+    std::cout << "Result: " << res << "\n";
+
     return 0;
 }

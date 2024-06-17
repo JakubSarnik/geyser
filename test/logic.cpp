33e3b7dc9dd2588fb9c9108f908ae016abb99b82
Jakub Šárník <jakub.sarnik@gmail.com>
2024-06-17 02:26:33 +0200

    Test formula mapping.

--- test/logic.cpp
+++ test/logic.cpp
@@ -305,3 +305,55 @@ TEST_CASE( "CNF formula is built correctly using add_cnf" )
 
     REQUIRE( to_nums( f1 ) == std::vector{ 1, 2, 2, 0, -2, 0, 1, 0, 2, -3, 0 } );
 }
+
+TEST_CASE( "Formulas are mapped correctly" )
+{
+    auto store = variable_store{};
+    auto f = cnf_formula{};
+
+    const auto a = literal{ store.make() };
+    const auto b = literal{ store.make() };
+    const auto c = literal{ store.make() };
+
+    f.add_clause( a, b, b );
+    f.add_clause( !b, a, c );
+    f.add_clause( !c, c );
+
+    REQUIRE( to_nums( f ) == std::vector{ 1, 2, 2, 0, -2, 1, 3, 0, -3, 3, 0 } );
+
+    SECTION( "To a constant" )
+    {
+        const auto to_ten = []( literal )
+        {
+            return literal{ variable{ 10 } };
+        };
+
+        const auto to_neg_ten = []( literal )
+        {
+            return !literal{ variable{ 10 } };
+        };
+
+        REQUIRE( to_nums( f.map( to_ten ) ) == std::vector{ 10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 0 } );
+        REQUIRE( to_nums( f.map( to_neg_ten ) ) == std::vector{ -10, -10, -10, 0, -10, -10, -10, 0, -10, -10, 0 } );
+    }
+
+    SECTION( "Using a constant substitution" )
+    {
+        const auto to_ten = []( literal lit )
+        {
+            return lit.substitute( variable{ 10 } );
+        };
+
+        REQUIRE( to_nums( f.map( to_ten ) ) == std::vector{ 10, 10, 10, 0, -10, 10, 10, 0, -10, 10, 0 } );
+    }
+
+    SECTION( "Using a non-constant substitution" )
+    {
+        const auto inc = []( literal lit )
+        {
+            return lit.substitute( variable{ lit.var().id() + 1 } );
+        };
+
+        REQUIRE( to_nums( f.map( inc ) ) == std::vector{ 2, 3, 3, 0, -3, 2, 4, 0, -4, 4, 0 } );
+    }
+}

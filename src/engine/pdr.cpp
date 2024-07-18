4a71d18de712c0e3fa8e70f82899665f68c35934
Jakub Šárník <jakub.sarnik@gmail.com>
2024-07-18 03:57:12 +0200

    Start implementation of PDR/solver infrastructure.

--- src/engine/pdr.cpp
+++ src/engine/pdr.cpp
@@ -8,7 +8,39 @@ result pdr::run( const transition_system& system )
     _system = &system;
     const auto bound = _opts->bound.value_or( std::numeric_limits< int >::max() );
 
+    initialize();
+
     return unknown{};
+}
+
+void pdr::initialize()
+{
+    push_frame();
+
+    _transition_activator = literal{ _store->make( "ActT" ) };
+    _error_activator = literal{ _store->make( "ActE" ) };
+
+    _activated_init = _system->init().activate( _trace[ 0 ].activator.var() );
+    _activated_trans = _system->trans().activate( _transition_activator.var() );
+    _activated_error = _system->error().activate( _error_activator.var() );
+}
+
+void pdr::refresh_solver()
+{
+    trace( "Refreshing the solver" );
+
+    assert( _system );
+
+    _solver = std::make_unique< CaDiCaL::Solver >();
+    _queries = 0;
+
+    assert_formula( _activated_init );
+    assert_formula( _activated_trans );
+    assert_formula( _activated_error );
+
+    for ( const auto& frame : _trace )
+        for ( const auto& cube : frame.blocked_cubes )
+            assert_formula( cube.negate().activate( frame.activator.var() ) );
 }
 
 } // namespace geyser::pdr
--- src/engine/pdr.hpp
+++ src/engine/pdr.hpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <memory>
 #include <vector>
+#include <span>
 
 namespace geyser::pdr
 {
@@ -50,15 +51,106 @@ public:
 
 class pdr : public engine
 {
+    class query_builder
+    {
+        CaDiCaL::Solver* _solver;
+
+    public:
+        explicit query_builder( CaDiCaL::Solver& solver ) : _solver{ &solver } {}
+
+        query_builder( const query_builder& ) = delete;
+        query_builder( query_builder&& ) = delete;
+
+        query_builder& operator=( const query_builder& ) = delete;
+        query_builder& operator=( query_builder&& ) = delete;
+
+        ~query_builder() = default;
+
+        void assume( literal l )
+        {
+            _solver->assume( l.value() );
+        }
+
+        void assume( std::span< literal > literals )
+        {
+            for ( const auto l : literals )
+                assume( l );
+        }
+
+        void constrain( const cnf_formula& clause )
+        {
+            assert( std::ranges::count( clause.literals(), literal::separator ) == 1 );
+
+            for ( const auto l : clause.literals() )
+                _solver->constrain( l.value() );
+        }
+
+        bool is_sat()
+        {
+            const auto res = _solver->solve();
+            assert( res != CaDiCaL::UNKNOWN );
+
+            return res == CaDiCaL::SATISFIABLE;
+        }
+    };
+
     struct frame
     {
+        std::vector< ordered_cube > blocked_cubes;
+        literal activator;
 
+        explicit frame( literal activator ) : activator{ activator } {}
     };
 
     using engine::engine;
 
     std::unique_ptr< CaDiCaL::Solver > _solver;
     const transition_system* _system = nullptr;
+
+    literal _transition_activator;
+    literal _error_activator;
+
+    cnf_formula _activated_init; // This is activated by _trace[ 0 ].activator
+    cnf_formula _activated_trans;
+    cnf_formula _activated_error;
+
+    std::vector< frame > _trace;
+
+    // How many solver queries to make before refreshing the solver to remove
+    // all the accumulated subsumed clauses.
+    constexpr static int solver_refresh_rate = 5000;
+    int _queries = 0;
+
+    void refresh_solver();
+
+    [[nodiscard]] query_builder with_solver()
+    {
+        if ( _queries++ % solver_refresh_rate == 0 )
+            refresh_solver();
+
+        assert( _solver );
+        return query_builder{ *_solver };
+    }
+
+    void assert_formula( const cnf_formula& formula )
+    {
+        assert( _solver );
+
+        for ( const auto lit : formula.literals() )
+            _solver->add( lit.value() );
+    }
+
+    [[nodiscard]] int k() const
+    {
+        return (int) _trace.size() - 1;
+    }
+
+    void push_frame()
+    {
+        _trace.emplace_back( literal{ _store->make( std::format( "Act[{}]", k() + 1 ) ) } );
+    }
+
+    void initialize();
 
 public:
     [[nodiscard]] result run( const transition_system& system ) override;
